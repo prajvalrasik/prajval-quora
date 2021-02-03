ecf825419854659f713ae10777204199f0125428
Prajval Rasik <believeyourself235@gmail.com>
2021-02-04 01:04:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Quora from "./component/Quora";
+
+function App() {
+  return (
+    <div className="App">
+      Quora
+      <Quora />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Quora.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./css/Quora.css";
+import Navbar from "./Navbar";
+
+
+function Quora() {
+    return (
+        <div className ="quora">
+            Hello It's Quora Container
+            <Navbar />
+        </div>
+    )
+}
+
+export default Quora
